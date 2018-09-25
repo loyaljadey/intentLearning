620cf6ca572c5e75761734e630a2483fef372024
Michael Jia <loyaljadey@gmail.com>
2018-09-25 09:36:27 -0700

    created the intentshare in the sendmessageactivity

--- app/src/main/java/com/example/intentlearning/SendMessageActivity.java
+++ app/src/main/java/com/example/intentlearning/SendMessageActivity.java
@@ -11,11 +11,13 @@ public class SendMessageActivity extends AppCompatActivity {
 
     private Button buttonSendMessage;
     private EditText editTextMessage;
+    private Button buttonShareMessage;
 
     public static final String EXTRA_SENT_MESSAGE = "the message";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_message);
         wireWidgets();
@@ -34,12 +36,28 @@ public class SendMessageActivity extends AppCompatActivity {
                 startActivity(intentSendMessage);
             }
         });
+
+        buttonShareMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //make an intent with the desired action
+                Intent intentShare = new Intent(Intent.ACTION_SEND);
+                //set the data type of the stuff we are packaging away
+                //look up the type online
+                intentShare.setType("text/plain");
+                //put the extra wiht the message
+                intentShare.putExtra(EXTRA_SENT_MESSAGE, editTextMessage.getText().toString());
+                //launch activity
+                startActivity(intentShare);
+            }
+        });
     }
 
     private void wireWidgets()
     {
         buttonSendMessage = findViewById(R.id.button_sendmessage_send);
         editTextMessage = findViewById(R.id.editText_sendmessage_msg);
+        buttonShareMessage = findViewById(R.id.button_sendmessage_share);
     }
 
 
